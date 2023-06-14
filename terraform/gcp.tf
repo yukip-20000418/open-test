3d35dfe8fc65fa84f1c2f1d5dd2e1adc76f98159
yukip-chottodake <yukip@chottodake.dev>
2023-06-14 01:09:36 +0000

    provider beta & secret key

--- terraform/gcp.tf
+++ terraform/gcp.tf
@@ -1,5 +1,11 @@
-provider "google" {
+provider "google-beta" {
   project = "dev-chottodake-open-test"
   region  = "asia-northeast1"
 #   zone    = "asia-northeast1-a"
+  credentials = data.google_secret_manager_secret_version.key_json.secret_data
+}
+
+data "google_secret_manager_secret_version" "key_json" {
+  project   = "dev-chottodake-open-test"
+  secret = "terraform-key-json"
 }
