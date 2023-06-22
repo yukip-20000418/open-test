e9474d3d7cddb278b27025b80709ea8299a04e76
yukip-chottodake <yukip@chottodake.dev>
2023-06-22 18:54:32 +0000

    sh change & use tmp

--- disable-services.sh
+++
@@ -1,2 +0,0 @@
-# Secret Manager API
-gcloud services disable secretmanager.googleapis.com --force
--- init-open-test.sh
+++ init-open-test.sh
@@ -1,20 +1,30 @@
+#!/bin/bash
+set -v
+
 # set project
 gcloud config set project dev-chottodake-open-test
 
-# api & service
+
+# api & service list
 gcloud services list --format='csv(TITLE,NAME)' \
 | awk 'NR>=2 {print}' \
 | sort \
 | awk -F, '{printf("# %s\n%s %s %s\n",$1,"gcloud services disable",$2,"--force")}' \
-> disable-services.sh
-chmod +x disable-services.sh
-./disable-services.sh
+> /tmp/disable-services.sh
 
+
+# api & service disable
+bash -v /tmp/disable-services.sh
+
+
+# api & service enable
 gcloud services enable serviceusage.googleapis.com
+
 
 # service account
 gcloud iam service-accounts delete terraform@dev-chottodake-open-test.iam.gserviceaccount.com --quiet
-gcloud iam service-accounts create terraform \
+
+gcloud iam service-accounts create terraform
 
 gcloud projects add-iam-policy-binding dev-chottodake-open-test \
 --member="serviceAccount:terraform@dev-chottodake-open-test.iam.gserviceaccount.com" \
@@ -23,13 +33,18 @@ gcloud projects add-iam-policy-binding dev-chottodake-open-test \
 gcloud iam service-accounts keys create /tmp/terraform-key.json \
 --iam-account="terraform@dev-chottodake-open-test.iam.gserviceaccount.com"
 
+
 # secret manager
 gcloud services enable secretmanager.googleapis.com
+
 sleep 5
+
 gcloud secrets delete terraform-key-json --quiet
+
 gcloud secrets create terraform-key-json --quiet \
 --replication-policy="automatic" \
 --data-file="/tmp/terraform-key.json"
+
 
 # create buckets for tfstate
 gcloud storage buckets create gs://test.open.chottodake.dev \
