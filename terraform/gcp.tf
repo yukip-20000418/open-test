03cacd42542eabb92e2e2ba644005186f26dc163
yukip-chottodake <yukip@chottodake.dev>
2023-06-26 13:52:14 +0000

    dekita kamo

--- terraform/gcp.tf
+++ terraform/gcp.tf
@@ -1,11 +1,12 @@
-provider "google-beta" {
+# provider "google-beta" {
+provider "google" {
   project = "dev-chottodake-open-test"
   region  = "asia-northeast1"
 #   zone    = "asia-northeast1-a"
-  credentials = data.google_secret_manager_secret_version.key_json.secret_data
+#   credentials = data.google_secret_manager_secret_version.key_json.secret_data
 }
 
-data "google_secret_manager_secret_version" "key_json" {
-  project   = "dev-chottodake-open-test"
-  secret = "terraform-key-json"
-}
+# data "google_secret_manager_secret_version" "key_json" {
+#   project   = "dev-chottodake-open-test"
+#   secret = "terraform-key-json"
+# }
